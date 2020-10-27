e288d5a05d6f1176c53c15133a4a10b1624ac6de
stigus96 <stighelgebruset@msn.com>
2020-10-27 15:40:26 +0100

    configured authservice

---
+++ CheckList/src/main/java/com/example/checklist/RunOnStartup.java
@@ -0,0 +1,36 @@
+package com.example.checklist;
+
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author mikael
+ */
+@Singleton
+@Startup
+public class RunOnStartup {
+    @PersistenceContext
+    EntityManager em;
+
+
+    
+    @PostConstruct
+    public void init() {
+        System.out.println("Wrrooom! " + new Date());
+        long groups = (long) em.createQuery("SELECT count(g.name) from Group g").getSingleResult();
+        if(groups == 0) {
+            em.persist(new Group(Group.USER));
+            em.persist(new Group(Group.ADMIN));
+        }
+        
+        long users = (long) em.createQuery("SELECT count(u.userid) from User u").getSingleResult();
+
+        }
+    }
+
